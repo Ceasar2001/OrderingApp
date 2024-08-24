55df09435747b5c93671ece8472afe24f7bbdf11
Ceasar2001 <ecleoceasar1@gmail.com>
2024-08-24 13:29:57 +0800

    managing the linking of screens

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,7 @@
-import { FlatList } from 'react-native';
+import { Redirect } from 'expo-router';
 
-import products from '@/assets/data/products';
-import ProductListItem from '@/src/components/ProductListItem';
-
-export default function MenuScreen() {
-  return (
-        <FlatList
-          data={products}
-          renderItem={({ item }) => <ProductListItem product={item} /> }
-          numColumns={2}
-          contentContainerStyle={{gap: 10, padding: 10}}
-          columnWrapperStyle={{gap: 10}}
-        />
-  );
+export default function TabIndex(){
+    return (
+        <Redirect href={'/menu/'} />
+    );
 }
-
-//The FlatList is used for displaying a scrollable list of products
-// the numColumns will arrange the products in grids
