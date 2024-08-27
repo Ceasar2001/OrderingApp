64c898ae2d2fe34634b5a5fac4d0219b4d39290a
Ceasar2001 <ecleoceasar1@gmail.com>
2024-08-27 21:51:55 +0800

    adding images to create product

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,13 +1,19 @@
-import { View, Text, StyleSheet, TextInput} from 'react-native'
+import { View, Text, StyleSheet, TextInput, Image} from 'react-native'
 import React, { useState } from 'react'
 import Button from '@/src/components/Button';
+import { defaultPizzaImage } from '@/src/components/ProductListItem';
+import Colors from '@/src/constants/Colors';
+import * as ImagePicker from 'expo-image-picker';
+import { Stack } from 'expo-router';
+
 
 const CreateProductScreen = () => {
 
 const [name, setName] = useState('');
 const [price, setPrice] = useState('');
-
 const [errors, setErrors] = useState('');
+const [image, setImage] = useState <string | null> (null);
+
 
 const resetFields = () => {
     setName('');
@@ -15,6 +21,7 @@ const resetFields = () => {
 };
 
 const validateInput = () => {
+    setErrors('');
     if(!name){
         setErrors('Name is required');
         return false;
@@ -42,8 +49,32 @@ const onCreate = () => {
     //save on the database
 };
 
+//expo image picker author link
+//https://docs.expo.dev/versions/latest/sdk/imagepicker/
+
+const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
+        <Stack.Screen options={{ title: 'Create product'}}/>
+
+        <Image source={{ uri: image || defaultPizzaImage }} style={styles.image} />
+        <Text onPress={pickImage} style={styles.textButton}>Select Image</Text>
+
       <Text style={styles.label}>Product Name</Text>
       <TextInput 
             value={name} 
@@ -60,7 +91,7 @@ const onCreate = () => {
             style={styles.input} 
             keyboardType='numeric'/>
 
-        <Text style={{ color: 'red '}}>{errors}</Text>
+        <Text style={{ color: 'red'}}>{errors}</Text>
 
       <Button onPress={onCreate} text='Create' />
     </View>
@@ -72,6 +103,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         padding: 10,
+    },
+
+    image:{
+        width: '50%',
+        aspectRatio: 1,
+        alignSelf: 'center',
+    },
+
+    textButton:{
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: Colors.light.tint,
+        marginVertical: 10,
     },
 
     input:{
